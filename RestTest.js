c6fd88159f95d626c941b78b4a5ae80cef68421d
Mark Cheli <mpcheli7@gmail.com>
2017-01-18 08:32:09 -0500

    First Commit

---
+++ RestTest.js
@@ -0,0 +1,29 @@
+var http = require("http");
+
+var options = {
+  "method": "GET",
+  "hostname": "academic-dev2.cloud.thingworx.com",
+  "port": null,
+  "path": "/Thingworx/Things/SocratesChild/Properties/name",
+  "headers": {
+    "appkey": "53253c29-f4d4-4718-89dc-349941150dc4",
+    "content-type": "application/json",
+    "cache-control": "no-cache",
+    "postman-token": "f2b5a8fc-0fea-9926-b74c-86d42fc0c563"
+  }
+};
+
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.end();
